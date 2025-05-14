ef305ab2da0410d4d5b0fdc4b9cf70b652738562
frske <144455292+frske@users.noreply.github.com>
2025-05-14 10:07:54 +0200

    basic webserver

--- implementation/main.py
+++ implementation/main.py
@@ -2,8 +2,22 @@
 # Main entry point for the Face Frenzy game
 
 from system import FaceFrenzySystem
+from bottle import route, run
 
 if __name__ == "__main__":
     print("Starting Face Frenzy game...")
     system = FaceFrenzySystem()
     system.run()
+
+
+@route("/")
+def info():
+    information = f"""
+    Score: {system.final_score}
+    Round:
+    Strikes: {system.game_loop.strikes}
+    Face count: {str(system.game_loop.detected_faces)} 
+    Game state: {system.state}
+    """
+
+    return information
